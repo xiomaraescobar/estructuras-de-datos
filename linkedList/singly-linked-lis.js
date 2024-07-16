0803f692469d56b209d9765b2798e6ee9472eb27
Your Name <you@example.com>
2024-07-16 09:52:58 -0500

    doubly LinkedList

--- linkedList/singly-linked-lis.js
+++ linkedList/singly-linked-lis.js
@@ -88,3 +88,114 @@ class MySinglyLinkedList {
 }
 
 let myLinkedList = new MySinglyLinkedList(1);
+
+
+// node con doublyLinkedList
+
+class  Node {
+  constructor(value) {
+      this.value = value;
+      this.next = null;
+      this.prev = null;
+      
+  }
+}
+
+class MyDoublyLinkedList {
+  constructor(value){
+      this.head = {
+          value: value,
+          next: null,
+          prev: null,
+      };
+      this.tail = this.head;
+      this.length = 1;
+  }
+
+append (value){
+  const newNode = new Node (value);
+  newNode.prev = this.tail;
+  this.tail.next = newNode; 
+  this.tail = newNode;
+  this.length++;
+  return this;
+  }
+
+  prepend (value){
+      if (this.length === 1){
+      const newNode = new Node (value);
+      newNode.prev = this.tail;
+      this.tail.next = newNode; 
+      this.tail = newNode;
+
+      this.length++;        
+      return this;
+  }
+      const newNode = new Node (value);
+      newNode.prev =  null;
+      newNode.next = this.head;
+      this.head = newNode;
+
+      this.length++;        
+      return this;
+  }
+
+      insert (index, value){
+          if(index >= this.length){
+              return this.append(value);
+          }
+
+          const newNode = new Node (value);
+          const firstPointer = this.getTheIndex(index - 1);
+          const holdingPointer = firstPointer.next;  
+          firstPointer.next = newNode;
+          newNode.next = holdingPointer;
+          newNode.prev = firstPointer;
+          this.length++;
+          return this;
+      } 
+      //counter
+      getTheIndex(index){
+          let counter = 0;
+          let currentNode = this.head; 
+          
+          while (counter !== index){
+              currentNode = currentNode.next;
+              counter++;
+          }
+          return currentNode;
+      }
+      remove (index){
+          if(index >= this.length){
+              return 'Este nodo no existe';
+          }
+          else if (index === 0){
+              const deleteNode = this.getTheIndex(index);
+              deleteNode.prev = this.tail;
+
+              this.tail = deleteNode;
+              const afterDelete = this.getTheIndex(index + 1);     
+              this.head = afterDelete;
+              this.tail.next = null; 
+              afterDelete.prev = null;
+
+              return this;
+          }
+          else{
+              const deleteNode = this.getTheIndex(index);
+              deleteNode.prev = this.tail;
+              this.tail = deleteNode;
+              
+              const afterDelete = this.getTheIndex(index + 1);     
+              const beforeDelete = this.getTheIndex(index - 1);        
+              beforeDelete.next=afterDelete;
+
+              this.tail.next = null; 
+              afterDelete.prev = beforeDelete;
+              
+          return this;
+          } 
+  }
+  } 
+  let myDoublyLinkedList = new MyDoublyLinkedList(1);
+
